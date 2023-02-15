8dc5bee9e1106350c7d71ba738e8928d6bd7aa1b
bekzodkeldiyarov <bek.bek.bek.bek72@gmail.com>
2023-02-15 13:03:19 +0500

    Done

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,31 @@
 package com.epam.mjc.nio;
 
+import java.io.BufferedReader;
 import java.io.File;
-
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
 
 public class FileReader {
-
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        try (BufferedReader reader = Files.newBufferedReader(file.toPath())) {
+            String currentLine = "";
+            Map<String, String> data = new HashMap<>();
+            while ((currentLine = reader.readLine()) != null) {
+                String[] keyValue = currentLine.split(": ");
+                data.put(keyValue[0].toLowerCase(), keyValue[1]);
+            }
+            System.out.println(data);
+            Profile profile = new Profile(
+                    data.get("name"),
+                    Integer.parseInt(data.get("age")),
+                    data.get("email"),
+                    Long.parseLong(data.get("phone"))
+            );
+            return profile;
+        } catch (IOException e) {
+            return new Profile();
+        }
     }
 }
